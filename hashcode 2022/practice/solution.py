675a05cf5b8a80663e9327574488aaac3f3a4e7f
Donutson <46778033+Donutson@users.noreply.github.com>
2022-01-08 19:42:30 +0000

    change: comments

--- hashcode 2022/practice/solution.py
+++ hashcode 2022/practice/solution.py
@@ -74,7 +74,7 @@ def solution(C, P, filepath):
 def main():                                        #
 	### Here goes the action you want to do ########  
 	base = Base(samples, get_entries, solution)    #
-	base.create_submissions()                      #
+	# base.create_submissions()                    #
 	# base.execute_solution(("A"))                 #
                                                        #
 if __name__ == "__main__":                             #
